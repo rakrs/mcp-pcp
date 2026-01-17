c5a1e069aefe74d2e6a84b9d594e0df1783889ec
rakrs <rakrs1@gmail.com>
2026-01-17 17:55:10 -0300

    feat: Add initial configuration for database URL and JWT secret.

--- core/config.py
+++ core/config.py
@@ -1,4 +1,4 @@
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://dummy")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgres://postgres:rFbwXV3N5diojFQtLfmZOMBDlHgpBFeZHxBlAuqDWkg8S7GAx46Cgu07s5YIKC6K@jcgkc0s44wkowcogwcc8gswk:5432/postgres")
 JWT_SECRET = os.getenv("JWT_SECRET", "changeme")
