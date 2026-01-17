67918241d058dddfffe0b51d9f17ce143066d6d7
rakrs <rakrs1@gmail.com>
2026-01-17 18:00:11 -0300

    correcao arquivo

--- core/database.py
+++ core/database.py
@@ -2,6 +2,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from core.config import DATABASE_URL
 
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine)
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL não definida")
+
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
